748965392ff14acda0db49c8a95cfde7574c3779
muhdhaniff55 <44047948+muhdhaniff55@users.noreply.github.com>
2024-09-27 22:39:05 +0800

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -15,7 +15,7 @@ device.name2=bhima
 device.name3=
 device.name4=
 device.name5=
-supported.versions=11 - 14
+supported.versions=11 - 15
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
